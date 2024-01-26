48ecc3c2381c6983ff347c45edcfb7977a5ec2aa
James Dempsey <james2.dempsey@live.uwe.ac.uk>
2024-01-26 22:34:24 +0000

    Fridge Runner Base added

---
+++ Assets/FridgeRunnerAssets/Scripts/BasicMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+
+    private void Update()
+    {
+        transform.position += new Vector3(0, 0, (float)-0.1);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit");
+        if (other.gameObject.name.Equals("Dangerzone"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/FridgeRunnerAssets/Scripts/ObstacleControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleControl : MonoBehaviour
+{
+    public GameObject fridge;
+
+    public GameObject obstacle;
+    public GameObject obstacle2;
+    public GameObject obstacle3;
+
+    void Update()
+    {
+     
+    }
+}
