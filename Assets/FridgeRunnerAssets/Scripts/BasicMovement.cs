00894cbb933d2808dbf5c7d52a81ab2d1d86b6b9
Ben Alford <benjamin3.alford@live.uwe.ac.uk>
2024-01-28 13:41:32 +0000

    Revert "Merge branch 'main' into test"
    This reverts commit 69df07175d7879a1c42cb8886eb03dccf2b50072.

--- Assets/FridgeRunnerAssets/Scripts/BasicMovement.cs
+++ Assets/FridgeRunnerAssets/Scripts/BasicMovement.cs
@@ -5,83 +5,93 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    public GameObject fridge;
-    public GameObject floor;
-
     bool movement = true;
 
-    //a +   d -
     float targetXpos = -1000;
-    float stepLength = 0.2f;
-    float movementSpeed = 52.2f;
-
-    private void Start()
-    {
-        transform.position = new Vector3(0, -2, 40);
-    }
+    float stepLength = 0.5f;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        //a +   d -
+        int movementSpeed = 50;
+
+        if (gameObject.name.Equals("Player"))
         {
-            if (transform.position.x == -4)
+            if (Input.GetKeyDown(KeyCode.A))
             {
-                targetXpos = 0;
+                if (transform.position.x == -4)
+                {
+                    targetXpos = 0;
+                }
+                else if (transform.position.x == 0)
+                {
+                    targetXpos = 4;
+                }
+                stepLength = 0.5f;
             }
-            else if (transform.position.x == 0)
+
+
+            if (Input.GetKeyDown(KeyCode.D))
             {
-                targetXpos = 4;
+                if (transform.position.x == 4)
+                {
+                    targetXpos = 0;
+                }
+                else if (transform.position.x == 0)
+                {
+                    targetXpos = -4;
+                }
+                stepLength = -0.5f;
             }
-            stepLength = 0.5f;
+
+
+            if (movement)
+            {
+                if (targetXpos == -1000 | targetXpos == transform.position.x)
+                {
+                    transform.position += new Vector3(0, 0, -movementSpeed * Time.deltaTime);
+                    stepLength = 0f;
+                }
+                else
+                {
+                    transform.position += new Vector3(stepLength, 0, -movementSpeed * Time.deltaTime);                             
+                }
+            }
         }
 
-
-        if (Input.GetKeyDown(KeyCode.D))
+        if (gameObject.name.Equals("Fridge"))
         {
-            if (transform.position.x == 4)
-            {
-                targetXpos = 0;
-            }
-            else if (transform.position.x == 0)
-            {
-                targetXpos = -4;
-            }
-            stepLength = -0.5f;
-        }
-
-
-        if (movement)
-        {
-            if (targetXpos == -1000 | targetXpos == transform.position.x)
+            if (movement)
             {
                 transform.position += new Vector3(0, 0, -movementSpeed * Time.deltaTime);
-                stepLength = 0f;
-            }
-            else
-            {
-                transform.position += new Vector3(stepLength, 0, -movementSpeed * Time.deltaTime);
             }
         }
 
-        if (transform.position.z < -300)
-        {
-            floor.transform.SetParent(gameObject.transform);
-        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name.Equals("Dangerzone"))
         {
-            transform.GetChild(0).SetParent(null, true);
-            movement = false;
-            gameObject.SetActive(false);
+            PlayerLoses(); 
         }
 
         if (other.gameObject.name.Equals("BlockStop"))
         {
-            Debug.Log("End");
-            floor.transform.SetParent(transform);
+            PlayerWins();
         }
+    }
+
+    void PlayerLoses()
+    {
+        transform.GetChild(0).SetParent(null, true);
+        Destroy(gameObject);
+    }
+
+    void PlayerWins()
+    {
+        Debug.Log("Win");
+        movement = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
     }
 }
